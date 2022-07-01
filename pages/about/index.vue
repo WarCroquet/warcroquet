0a2a678739bb05e9a0d40ee0d76a14ed2c7ffb37
JacobAnneberg <jacobanneberg@gmail.com>
2022-07-01 23:32:56 +0200

    Added about text

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,20 @@
 <template>
-  NOT IMPLEMENTED
+  <div>
+    <p>
+      Created by the HTX Bois.
+
+      Erik Thomsen aka "Den sleske"
+      Jacob Anneberg Sørensen aka "Mr. Jetset"
+      Jonas Niaraki Jensen aka "Swahili"
+      Marcus Astrup Hansen aka "Tarzan wannabe"
+      Mikkel Fjord Olsen aka "Brøleabe"
+      Sigurd Eugen Espersen aka "TT"
+      Toke Rosenbæk Andersen aka "Tokxic"
+      Troels Zink Kristensen aka "Tics boy 2000"
+
+      Copyright © 2022
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
