bd8883eb058ae7d7bd42a65f8ffd8809fef2b3c0
SigurdEEspersen <32567367+SigurdEEspersen@users.noreply.github.com>
2022-09-28 20:20:58 +0200

    Misc changes

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -2,6 +2,7 @@
   <h1 class="text-white font-bold text-center mt-5 mb-5 text-xl">
     BATTLE ARENA
   </h1>
+  <p class="text-white text-center">ROUND: {{ this.roundCounter }}</p>
 
   <div class="flex flex-wrap text-white justify-center mt-4">
     <PlayerTemplate
@@ -15,10 +16,29 @@
   </div>
 
   <div class="flex flex-wrap justify-center">
-    <button @click="stun_toggle()" class="btn btn-yellow-ability">STUN</button>
-    <button @click="cooldown_toggle()" class="btn btn-yellow-ability">
-      CD
+    <button
+      id="stunBtn"
+      @click="stun_toggle()"
+      class="btn btn-yellow-ability opacity-70"
+    >
+      <img
+        src="~/assets/stun-icon.png"
+      /><!--Sæt den til venstre for teksten og gør den mindre-->
+      STUN
     </button>
+
+    <button
+      id="cooldownBtn"
+      @click="cooldown_toggle()"
+      class="btn btn-yellow-ability opacity-70"
+    >
+      <img
+        src="~/assets/ability-icon.png"
+      /><!--Sæt den til venstre for teksten og gør den mindre-->
+      COOLDOWN
+      <!--Den gider ikke aligne teksten??-->
+    </button>
+
     <button @click="nextRound_click()" class="btn btn-yellow-ability">
       NEXT
     </button>
@@ -44,12 +64,15 @@ export default {
       players: [],
       stunToggle: Boolean,
       cooldownToggle: Boolean,
+      roundCounter: Number,
     };
   },
   mounted() {
+    // Ikke gør dette hvis man bare refresher siden
     this.players = JSON.parse(localStorage.getItem("players")) as Player[];
-    this.stunToggle = false; // Hvis man refresher siden skal de ikke sættes til false
+    this.stunToggle = false;
     this.cooldownToggle = false;
+    this.roundCounter = 0;
   },
   methods: {
     player_click(player: Player) {
@@ -65,16 +88,37 @@ export default {
       }
     },
     stun_toggle() {
-      this.stunToggle = !this.stunToggle;
-      console.log("stunToggled: " + this.stunToggle);
+      this.setStunToggle(!this.stunToggle);
+    },
+    setStunToggle(toggle: Boolean) {
+      this.stunToggle = toggle;
+
+      if (this.stunToggle) {
+        document.getElementById("stunBtn").style.opacity = "1";
+      } else {
+        document.getElementById("stunBtn").style.opacity = "0.7";
+      }
     },
     cooldown_toggle() {
-      this.cooldownToggle = !this.cooldownToggle;
-      console.log("cooldownToggled: " + this.cooldownToggle);
+      this.setCooldownToggle(!this.cooldownToggle);
+    },
+    setCooldownToggle(toggle: Boolean) {
+      this.cooldownToggle = toggle;
+
+      if (this.cooldownToggle) {
+        document.getElementById("cooldownBtn").style.opacity = "1";
+      } else {
+        document.getElementById("cooldownBtn").style.opacity = "0.7";
+      }
     },
     nextRound_click() {
+      this.roundCounter++;
+      this.setStunToggle(false);
+      this.setCooldownToggle(false);
+
       this.players.forEach((player: Player) => {
         UpdateDecremantals(player);
+        // Opdater stun + cooldown tal på hver template.
         console.log(player.Name);
         console.log("Rounds stunned: " + player.RoundsStunned);
         console.log("Cooldown: " + player.Cooldown);
